16b581aaa3fb6fccf6119f1d823fc69d630f63c3
albrdev <albrdev@gmail.com>
2020-01-08 20:27:34 +0100

    added range check

--- alexander/src/geometry.c
+++ alexander/src/geometry.c
@@ -1,16 +1,35 @@
 #include "geometry.h"
+#include <errno.h>
 
 double getRectanglePerimeter(const double x, const double y)
 {
+    if(x <= 0.0 || y <= 0.0)
+    {
+        errno = ERANGE;
+        return -1.0;
+    }
+
     return (x * 2.0) + (y * 2.0);
 }
 
 double getRectangleArea(const double x, const double y)
 {
+    if(x <= 0.0 || y <= 0.0)
+    {
+        errno = ERANGE;
+        return -1.0;
+    }
+
     return x * y;
 }
 
 double getCuboidVolume(const double x, const double y, const double z)
 {
+    if(x <= 0.0 || y <= 0.0 || z <= 0.0)
+    {
+        errno = ERANGE;
+        return -1.0;
+    }
+
     return x * y * z;
 }
